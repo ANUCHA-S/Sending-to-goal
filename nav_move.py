c1606774103c432675b9136718509a31eaeb6298
ANUCHA-S <161399368+ANUCHA-S@users.noreply.github.com>
2024-03-01 10:10:01 +0700

    Add files via upload

---
+++ nav_move.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import actionlib
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseFeedback, MoveBaseResult
+
+rospy.init_node('send_client_goal')
+
+client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+rospy.loginfo("Waiting for move base server")
+client.wait_for_server()
+
+goal = MoveBaseGoal()
+goal.target_pose.header.frame_id = 'map' 
+goal.target_pose.pose.position.x = 0.3
+goal.target_pose.pose.position.y = 0.3
+goal.target_pose.pose.orientation.z = 0.0
+goal.target_pose.pose.orientation.w = 0.5
+
+client.send_goal(goal)
+client.wait_for_result()
+
+goal.target_pose.header.frame_id = 'map' 
+goal.target_pose.pose.position.x = 0.5
+goal.target_pose.pose.position.y = 0.5
+goal.target_pose.pose.orientation.z = 0.0
+goal.target_pose.pose.orientation.w = 1
+
+client.send_goal(goal)
+client.wait_for_result()
